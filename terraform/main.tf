443739ff1696ede3b0073c69e024d116b12a0e56
Sebastian-Andersson <sebastian@cshift.tech>
2023-08-05 13:26:44 +0200

    added fun stuff

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,11 +15,28 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "17.1.0"
 
-  tags = {
-    Name = "example-instance"
+  cluster_name    = "my-cluster"
+  cluster_version = "1.20"
+  subnets         = ["subnet-02972045bcbcaa2da", "subnet-0d5b74f041fe5b6b4"]
+  vpc_id          = "vpc-0da5b1500edceb34d"
+
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 10
+      min_capacity     = 1
+
+      instance_type = "t2.micro"
+      key_name      = "devops-demo"
+      
+      root_volume_size = 100
+      root_volume_type = "gp2"
+
+      ami_id = data.aws_ami.ubuntu.id
+    }
   }
 }
